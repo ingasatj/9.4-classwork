e862401eff4d60d5eaf3ffdbb369f8ef73645c6e
ingasatj <inga.sarj@gmail.com>
2025-06-15 19:54:27 +0400

    development

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Homework 4: Counter Button
 // Question: Create a button that counts how many times it has been clicked and displays the number.
-const countSpan = document.getElementById("count");
-const btn = document.getElementById("countBtn");
+const countSpan = document.querySelector("#count");
+const btn = document.querySelector("#countBtn");
 
 let count = 0;
 
